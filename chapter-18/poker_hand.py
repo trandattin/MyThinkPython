85eb3c16509aa55bde866a35edf6c8ebb5e4205a
trandattin <info@trandattin.com>
2020-02-18 11:50:23 +0700

    Update

--- chapter-18/poker_hand.py
+++ chapter-18/poker_hand.py
@@ -1,25 +1,60 @@
-"""This module contains code from
-Think Python by Allen B. Downey
-http://thinkpython.com
+from card import*
 
-Copyright 2012 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
+class Hist(dict):
+    def __init__(self, seq = []):
+        """A map from each time (x) to its frequency"""
+        for x in seq:
+            self.count(x)
 
-"""
-
-from card import *
+    def count(self,x, f=1):
+        """Increments the countter associated with item x"""
+        self[x] = self.get(x, 0 ) + f
+        if self[x] == 0:
+            del self[x]
 
 
 class PokerHand(Hand):
+    """Represents a poker hand."""
 
-    def suit_hist(self):
-        """Builds a histogram of the suits that appear in the hand.
+    all_labels = ['straightflush', 'fourkind', 'fullhouse','flust',
+                  'straight', 'threekind', 'twopair', 'pair', 'highcard']
 
-        Stores the result in attribute suits.
+    def make_histograms(self):
+        """Computes histograms for suits and hands
+
+        Creates attributes:
+
+        suits: a histogram of suits in the hand.
+        ranks: a histogram of the ranks
+        sets: a sorted list of the rank in the hand
         """
-        self.suits = {}
-        for card in self.cards:
-            self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
+
+        self.suits = Hist()
+        self.ranks = Hist()
+
+        for c in self.cards:
+            self.suits.count(c.suit)
+            self.rank.count(c.rank)
+
+        self.sets = self.ranks.values()
+        self.sets.sort(reverse=True)
+
+    def has_highcard(self):
+        """Returns True if this hand has a high card."""
+        return len(self.cards)
+
+    def check_sets(self, *t):
+        """Checks whether self.sets contains sets that are
+        at least as big as the requirements in t
+
+        t: list of int
+        """
+        for need, have in zip(t, self.sets):
+            if need > have: return False
+        return True
+
+    def has_pair(self):
+        return self.check_sets(2)
 
     def has_flush(self):
         """Returns True if the hand has a flush, False otherwise.
@@ -33,6 +68,35 @@ class PokerHand(Hand):
         return False
 
 
+    def has_two_pair(self):
+        self.rank_hist()
+        i = 0
+        for val in self.ranks.values():
+            if val == 2:
+                i +=1
+        if i == 2:
+            return True
+        return False
+
+    def has_three_of_kind(self):
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val == 3:
+                return True
+        return False
+
+    '''def has_straight(self):
+        for i in range(self.cards):
+           if self.card[i] - self.card[i+1]
+    '''
+
+    def has_full_house(self):
+        if self.has_three_of_kind() and self.has_pair():
+            return True
+        return False
+
+
+
 if __name__ == '__main__':
     # make a deck
     deck = Deck()
@@ -44,6 +108,6 @@ if __name__ == '__main__':
         deck.move_cards(hand, 7)
         hand.sort()
         print(hand)
-        print(hand.has_flush())
+        print(hand.has_full_house())
         print('')
 
